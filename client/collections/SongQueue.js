e9a31b73167a7e10e958665800bc7f5ed7e83dcf
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-01-30 10:36:10 -0800

    Add song to SongQueue collection on click

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,13 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function() {
+  initialize: function(song) {
+    this.on('change', this.enqueue, this);
+  },
+
+  enqueue: function(song) {
+    this.add(song);
+    console.info('SONG QUEUED')
   }
 
 });
