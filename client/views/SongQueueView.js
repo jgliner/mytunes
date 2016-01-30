3506d75a88e8bdac46ef08a941a1363d46bfb751
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-01-29 19:11:35 -0800

    Initialize SongQueue Views and enable corresponding tests

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,10 +1,22 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+
   initialize: function() {
+    this.render();
   },
 
   render: function() {
+    this.$el.children().detach();
+
+    if (this.collection !== undefined) {
+      this.$el.html('<th>Queue</th>').append(
+        this.collection.map(function(song) {
+          return new SongQueueEntryView({model: song}).render();
+        })
+      );
+    }
     return this.$el;
   }
 
